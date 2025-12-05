3b6c7caf44bbc46d5e2d86c53b5b9abcca9279e7
wdlhao <906144643@qq.com>
2025-12-05 15:20:30 +0800

    feat(test): person 1: hello b
    - person 1: hello b

--- src/components/HelloWorld1.vue
+++ src/components/HelloWorld1.vue
@@ -13,6 +13,7 @@ defineProps<{
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
     <span>person 1: hello a</span>
+    <span>person 1: hello b</span>
   </div>
 </template>
 
